caa1f1fe83aa916c7d70c3065819c95263df25c7
NikoMayz <mikeayoungjr@gmail.com>
2024-07-13 15:55:49 -0400

    completed seed files

---
+++ seeds/seed.js
@@ -0,0 +1,26 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+const userData = require('./users.json');
+const postData = require('./posts.json');
+const commentData = require('./comments.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Post.bulkCreate(postData, {
+    returning: true,
+  });
+
+  await Comment.bulkCreate(commentData, {
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
